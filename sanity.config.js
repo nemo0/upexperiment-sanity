3f3bd8d1abf34cd041e201b10066250eceb39bcd
Subha Chanda <subhachanda88@gmail.com>
2023-09-11 23:12:16 +0530

    chore: updated project ID

--- sanity.config.js
+++ sanity.config.js
@@ -5,9 +5,9 @@ import {schemaTypes} from './schemas'
 
 export default defineConfig({
   name: 'default',
-  title: 'upexperiment',
+  title: 'Upexperiment',
 
-  projectId: 'qizm92hu',
+  projectId: '888toz5o',
   dataset: 'production',
 
   plugins: [deskTool(), visionTool()],
